4e2bc3416bdf9a190a7ce6c3143daed2d6f6dd4e
Adrien Peiffer <adrien.peiffer@acsone.eu>
2022-04-13 13:51:43 +0200

    Backport account_invoice_payment_retention to 13.0

--- account_invoice_payment_retention/models/account_move.py
+++ account_invoice_payment_retention/models/account_move.py
@@ -97,12 +97,16 @@ class AccountMove(models.Model):
                 amount = rec.amount_retention
             elif rec.payment_retention == "percent":
                 # Ensure working with purchase deposit, sum only positive qty lines
-                amount_untaxed = sum(
-                    rec.invoice_line_ids.filtered(lambda l: l.quantity > 0).mapped(
-                        "amount_currency"
-                    )
-                )
-                sign = 1 if rec.move_type in ["in_invoice", "out_refund"] else -1
+                amount_untaxed = 0.0
+                for line in rec.invoice_line_ids.filtered(lambda l: l.quantity > 0):
+                    if line.move_id.type in line.move_id.get_outbound_types():
+                        sign = 1
+                    elif line.move_id.type in line.move_id.get_inbound_types():
+                        sign = -1
+                    else:
+                        sign = 1
+                    amount_untaxed += line.price_subtotal * sign
+                sign = 1 if rec.type in ["in_invoice", "out_refund"] else -1
                 amount = sign * (amount_untaxed * rec.amount_retention / 100)
             rec.retention_amount_currency = amount
 
@@ -126,7 +130,7 @@ class AccountMove(models.Model):
                 continue
             retained_moves = self._get_retained_move_lines(rec)
             retained = 0.0
-            sign = 1 if rec.move_type in ["in_invoice", "out_refund"] else -1
+            sign = 1 if rec.type in ["in_invoice", "out_refund"] else -1
             if rec.currency_id == rec.company_currency_id:
                 retained = sum(retained_moves.mapped("balance"))
             else:
