cf152a8b18bdeb997132709c8e2af936f4cc42ae
Jacques-Etienne Baudoux <je@bcim.be>
2024-04-29 16:44:40 +0200

    [FIX] account_invoice_transmit: credit note
    Get proper template for credit notes

--- account_invoice_transmit/models/account_move.py
+++ account_invoice_transmit/models/account_move.py
@@ -23,6 +23,9 @@ class AccountMove(models.Model):
 
     def _get_transmit_invoice_by_email_template(self):
         """Return email template used in mass sending"""
+        self.ensure_one()
+        if self.move_type == "out_refund":
+            return self.env.ref("account.email_template_edi_credit_note")
         return self.env.ref("account.email_template_edi_invoice")
 
     def _transmit_invoice(self, method):
