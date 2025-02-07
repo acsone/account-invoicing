6f4762a927727d0f4ab99cbd6859c629424d5e81
Florian da Costa <florian.dacosta@akretion.com>
2025-02-07 11:13:58 +0100

    [IMP] Hide sale_qty_to_reinvoice when the refund is not linked to sale Also take the company parameter in the default of the refund line

--- sale_line_refund_to_invoice_qty/models/account.py
+++ sale_line_refund_to_invoice_qty/models/account.py
@@ -20,7 +20,7 @@ class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
     sale_qty_to_reinvoice = fields.Boolean(
-        default=True,
+        default=lambda self: self.env.company.reinvoice_credit_note_default,
         help="Leave it marked if you will reinvoice the same sale order line",
         copy=False,
     )
